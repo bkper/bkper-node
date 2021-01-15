99d7aacc7b4e9fb3924c8e741f4ad6ed7c4409d9
mael <mael@bkper.com>
2021-01-15 17:24:24 -0300

    chore: Removing unuseful log

--- src/model/Transaction.ts
+++ src/model/Transaction.ts
@@ -310,7 +310,6 @@ export class Transaction {
    * 
    */
   public async getDebitAccount(): Promise<Account> {
-    console.log('BBBBB')
     return this.wrapped.debitAccount != null ? await this.book.getAccount(this.wrapped.debitAccount.id) : null;
   }
 
